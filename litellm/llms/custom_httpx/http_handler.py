14a751ef4a76398edb4a5cdfe081697cb4c11246
AlexsanderHamir <alexsanderhamirgomesbaptista@gmail.com>
2025-09-30 18:06:08 -0700

    fix old changes

--- litellm/llms/custom_httpx/http_handler.py
+++ litellm/llms/custom_httpx/http_handler.py
@@ -545,14 +545,20 @@ class AsyncHTTPHandler:
             - Some users have seen httpx ConnectionError when using ipv6 - forcing ipv4 resolves the issue for them
         """
         #########################################################
-        # HACK: FORCE AIOHTTP TRANSPORT ALWAYS
+        # AIOHTTP TRANSPORT is off by default
         #########################################################
-        return AsyncHTTPHandler._create_aiohttp_transport(
-            ssl_context=ssl_context,
-            ssl_verify=ssl_verify,
-            shared_session=shared_session,
-        )
+        if AsyncHTTPHandler._should_use_aiohttp_transport():
+            return AsyncHTTPHandler._create_aiohttp_transport(
+                ssl_context=ssl_context,
+                ssl_verify=ssl_verify,
+                shared_session=shared_session,
+            )
 
+        #########################################################
+        # HTTPX TRANSPORT is used when aiohttp is not installed
+        #########################################################
+        return AsyncHTTPHandler._create_httpx_transport()
+    
     @staticmethod
     def _should_use_aiohttp_transport() -> bool:
         """
